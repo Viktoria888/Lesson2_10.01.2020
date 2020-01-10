244f420ae5beb9124c8f273438fe46a8d58134d3
Никитенко Виктория Игоревна <@corp.itstep>
2020-01-10 18:09:12 +0600

    Add project files.

---
+++ Lesson2_10.01.2020/UserControl1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson2_10._01._2020
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string text = textBox1.Text;
+
+            MessageBox.Show(text);
+        }
+    }
+}
